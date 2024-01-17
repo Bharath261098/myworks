77b7d75e400a4799475f080b1d1e2bd529eaf76f
Bharath <bharathboss68367@gmail.com>
2024-01-17 14:01:08 +0530

    added

--- AppV4/main.py
+++ AppV4/main.py
@@ -1,105 +1,107 @@
+# Imports
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
 import os
-from dotenv import load_dotenv
-from htmlTemplates import css, user_template, bot_template
+from  apikey import apikey
 
+# Define OpenAI API KEY
+OPENAI_API_KEY = "28c49a2bd351436d8b85ac8693e25bd4"
 
-def init_ses_states():
-    st.session_state.setdefault('chat_history', [])
+# CSS styling
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 85%;
+  padding: 0 1.25rem;
+  color: #fff;
+}
+'''
+st.write(css, unsafe_allow_html=True)
 
+# Define user and bot templates
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://resizing.flixster.com/ocuc8yjm8Fu5UK5Ze8lbdp58m9Y=/300x300/v2/https://flxt.tmsimg.com/assets/p11759522_i_h9_aa.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
 
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://www.shutterstock.com/image-vector/nerd-robot-vector-illustration-version-260nw-2126944997.jpg">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+# App Title
+st.title("Stock Price Relative Returns AI Tool")
+st.caption("Visualizations and OpenAI Chatbot Comparing Multiple Stocks Over A Specified Period")
+
+# Define Stocke Drop Down Menu and Available Stock to choose from
+tickers = ['TSL', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD']
+dropdown = st.multiselect('Pick Asset:', tickers)
+
+start = st.date_input('Start', value=pd.to_datetime('2023-01-01'))
+end = st.date_input('End', value=pd.to_datetime('today'))
+
+# Define function to convert price to relative returns
 def relret(df):
     rel = df.pct_change()
     cumret = (1+rel).cumprod()-1
     cumret = cumret.fillna(0)
     return cumret
 
+# Only when a stock is selected
+if len(dropdown) > 0:
+    #df = yf.download(dropdown,start,end)['Adj Close']
+    df = relret(yf.download(dropdown,start,end)['Adj Close'])
+    st.header("Relative Returns of {}".format(dropdown))
+    st.line_chart(df)
+    st.header("Chat with your Data") 
 
-def display_convo():
-    with st.container():
-        for i, message in enumerate(reversed(st.session_state.chat_history)):
-            if i % 2 == 0:
-                 st.markdown(bot_template.replace("{{MSG}}", message), unsafe_allow_html=True)
-            else:
-                st.markdown(user_template.replace("{{MSG}}", message), unsafe_allow_html=True)
+     # Accept input from user
+    query = st.text_input("Enter a query:") 
 
+    # Execute pandas response logic
+    if st.button("Execute") and query:
+        with st.spinner('Generating response...'):
+            try:
 
-def main():
-    st.set_page_config(page_title="Stock Price AI Bot", page_icon=":chart:")
-    st.write(css, unsafe_allow_html=True)
-    init_ses_states()
-    st.title("Stock Price AI Bot")
-    st.caption("Visualizations and OpenAI Chatbot for Multiple Stocks Over A Specified Period")
+                 # Define pandas df agent - 0 ~ no creativity vs 1 ~ very creative
+                agent = create_pandas_dataframe_agent(OpenAI(temperature=0.0),pd.DataFrame(df),verbose=True) 
 
+                # Run agent and retrieve answer
+                answer = agent.run(query)
 
-    with st.sidebar:
-        asset_tickers = sorted(['DOW','NVDA','TSL','GOOGL','AMZN','AI','NIO','LCID','F','LYFY','AAPL', 'MSFT', 'BTC-USD', 'ETH-USD'])
-        asset_dropdown = st.multiselect('Pick Assets:', asset_tickers)
+                # Display user query and agents answer
+                st.write(user_template.replace("{{MSG}}",query ), unsafe_allow_html=True)
+                st.write(bot_template.replace("{{MSG}}", answer ), unsafe_allow_html=True)
+                st.write("")
 
-        metric_tickers = ['Adj. Close', 'Relative Returns']
-        metric_dropdown = st.selectbox("Metric", metric_tickers)
-
-        viz_tickers = ['Line Chart', 'Area Chart']
-        viz_dropdown = st.multiselect("Pick Charts:", viz_tickers)
-
-        start = st.date_input('Start', value=pd.to_datetime('2023-01-01'))
-        end = st.date_input('End', value=pd.to_datetime('today'))
-
-        chatbot_temp = st.slider("Chat Bot Temperature",0.0,1.0,0.5)
-
-
-    # Only when a stock is selected
-    if len(asset_dropdown) > 0:
-        df = yf.download(asset_dropdown,start,end)['Adj Close']
-        if metric_dropdown == 'Relative Returns':
-            df = relret(df)
-        if len(viz_dropdown) > 0:
-            with st.expander("Data Visualizations for {} of {}".format(metric_dropdown,asset_dropdown), expanded=True):
-                if "Line Chart" in viz_dropdown:
-                    st.subheader("Line Chart")
-                    st.line_chart(df)
-                if "Area Chart" in viz_dropdown:
-                    st.subheader("Area Chart")
-                    st.area_chart(df)
-        st.header("Chat with your Data") 
-
-        query = st.text_input("Enter a query:")
-
-        chat_prompt = f'''
-            You are an AI ChatBot intended to help with user stock data.
-            \nYou have access to a pandas dataframe with the following specifications 
-            \nDATA MODE: {metric_dropdown}
-            \nSTOCKS: {asset_dropdown} 
-            \nTIME PERIOD: {start} to {end}
-            \nCHAT HISTORY: {st.session_state.chat_history}
-            \nUSER MESSAGE: {query}
-            \nAI RESPONSE HERE:
-        '''
-
-        if st.button("Execute") and query:
-            with st.spinner('Generating response...'):
-                try:
-                    agent = create_pandas_dataframe_agent(
-                        ChatOpenAI(temperature=chatbot_temp, model='gpt-4'),
-                        pd.DataFrame(df),
-                        verbose=True
-                    )
-
-                    answer = agent.run(chat_prompt)
-                    st.session_state.chat_history.append(f"USER: {query}\n")
-                    st.session_state.chat_history.append(f"AI: {answer}\n")
-                    display_convo()
-
-                except Exception as e:
-                    st.error(f"An error occurred: {str(e)}")
-
-
-if __name__ == '__main__':
-    load_dotenv()
-    main()
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
